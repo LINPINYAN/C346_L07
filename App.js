bdda7f0fd974f3110b98aa978134f13ddd71e038
LINPINYAN <24019715@myrp.edu.sg>
2025-11-12 13:03:45 +0800

    Add exercises

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text} from 'react-native';
+import Exercise1 from './Exercise1.js';
+import Exercise2 from './Exercise2.js';
+import Exercise3A from './Exercise3A.js';
+import Exercise3B from './Exercise3B.js';
+import Exercise3C from './Exercise3C.js';
+import Exercise3D from './Exercise3D.js';
+import Exercise3E from './Exercise3E.js';
+import Exercise4 from './Exercise4.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MyApp = () => {
+    return (
+        <View style={{marginTop: 40, flex: 1}}>
+            <Text style={{fontSize: 30}}>Lesson 7 exercises:</Text>
+            {/*<Exercise1/>*/}
+            {/*<Exercise2/>*/}
+            {/*<Exercise3A/>*/}
+            {/*<Exercise3B/>*/}
+            {/*<Exercise3C />*/}
+            {/*<Exercise3D/>*/}
+            {/*<Exercise3E/>*/}
+            <Exercise4/>
+
+
+
+
+
+
+        </View>
+    );
+};
+
+export default MyApp;
